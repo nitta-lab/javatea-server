368a9e97a0b8d61dd14147765522bca7459bec52
n-nitta <n-nitta@konan-u.ac.jp>
2026-05-14 17:32:44 +0900

    Initial commit.

---
+++ src/main/java/org/nittalab/javateaserver/JavateaServerApplication.java
@@ -0,0 +1,13 @@
+package org.nittalab.javateaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavateaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavateaServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/nittalab/javateaserver/JavateaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.nittalab.javateaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavateaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
