886be754572b9dd0cc897669ba457e9d908ec63f
r-hashime <s2371079@s.konan-u.ac.jp>
2026-05-19 16:23:45 +0900

    User.java：Userメゾット・uidメゾットの実装 UserRepository.java：createUserメゾットの実装

--- src/main/java/org/nittalab/javateaserver/models/User.java
+++ src/main/java/org/nittalab/javateaserver/models/User.java
@@ -10,9 +10,36 @@ public class User {
     private String department;
     private int grade;
 
+    //渡されたuid, name, pwをフィールドに代入
+    public User(String uid, String name, String pw){
+        this.uid = uid;
+        this.name = name;
+        this.pw = pw;
+    }
+
+    //uidの取得
+    public String getUid(){
+        return uid;
+    }
+
+    //setUid
+    //getName
+    //setName
+    //getPw
+    //setPw
+    //getUniversity
+    //setUniversity
+    //getFaculty
+    //setFaculty
+    //getDepartment
+    //setDepartment
+    //getGrade
+    //setGrade
 
     //UserのTokenを返すメゾット
     public String getToken(){
         return null;
     }
+
+    //setToken
 }
--- src/main/java/org/nittalab/javateaserver/repositories/UserRepository.java
+++ src/main/java/org/nittalab/javateaserver/repositories/UserRepository.java
@@ -3,12 +3,17 @@ package org.nittalab.javateaserver.repositories;
 import org.nittalab.javateaserver.models.User;
 import org.springframework.stereotype.Repository;
 
+import java.util.HashMap;
+
 @Repository
 public class UserRepository {
+    private final HashMap<String, User> userMap = new HashMap<>(); //User情報を持つHashMap
 
     //Userクラスの作成
     public User createUser(String uid, String name, String pw){
-        return null;
+        User u = new User(uid, name, pw);
+        userMap.put(uid, u);
+        return u;
     }
 
     //Userクラスの取得
