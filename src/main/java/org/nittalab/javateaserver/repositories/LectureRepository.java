14b9d0b071ffa9a1fd82e826d627fba110d0f6f5
h-oonishi <s2371054@s.konan-u.ac.jp>
2026-05-19 15:32:45 +0900

    LectureRepositoryとLectureの中を作りました

--- src/main/java/org/nittalab/javateaserver/repositories/LectureRepository.java
+++ src/main/java/org/nittalab/javateaserver/repositories/LectureRepository.java
@@ -3,13 +3,28 @@ package org.nittalab.javateaserver.repositories;
 import org.nittalab.javateaserver.models.Lecture;
 import org.springframework.stereotype.Repository;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Set;
+
 @Repository
 public class LectureRepository {
-    public Lecture createLecture(String name, Integer grade, String semester, Integer frame, String day, Integer period){
-        return null;
+
+    private HashMap<String, Lecture> lectureMap = new HashMap<>();
+
+    //授業の追加
+    public String createLecture(String name, int grade, String semester, int frame, String day, int period) {
+        Set<String> keyList = lectureMap.keySet();
+        int num = keyList.size() + 1;
+        String lectureId = "Lecture-id" + num;
+        lectureMap.put(lectureId, new Lecture(name, grade, semester, frame, day, period));
+        return lectureId;
     }
-    public Lecture getLecture(String lectureId){
-        return null;
+
+
+    //授業の取得
+    public Lecture getLecture(String lectureId) {
+        return lectureMap.get(lectureId);
     }
 
 }
