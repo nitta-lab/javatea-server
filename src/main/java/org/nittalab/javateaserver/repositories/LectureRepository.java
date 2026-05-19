07198fc9d5ee0e35e27b5dfb684425be1706ad52
h-oonishi <s2371054@s.konan-u.ac.jp>
2026-05-19 14:01:43 +0900

    lecuturerepositoryの外面だけ作りました

--- src/main/java/org/nittalab/javateaserver/repositories/LectureRepository.java
+++ src/main/java/org/nittalab/javateaserver/repositories/LectureRepository.java
@@ -1,4 +1,15 @@
 package org.nittalab.javateaserver.repositories;
 
+import org.nittalab.javateaserver.models.Lecture;
+import org.springframework.stereotype.Repository;
+
+@Repository
 public class LectureRepository {
+    public Lecture createLecture(String name, Integer grade, String semester, Integer frame, String day, Integer period){
+        return null;
+    }
+    public Lecture getLecture(String lectureId){
+        return null;
+    }
+
 }
