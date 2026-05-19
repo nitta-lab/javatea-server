c0a4db26f1d45601e930b9786b2b96e25f47f0c6
r-hashime <s2371079@s.konan-u.ac.jp>
2026-05-19 15:10:04 +0900

    User.javaにUser情報を保持する変数を定義しました。

--- src/main/java/org/nittalab/javateaserver/models/User.java
+++ src/main/java/org/nittalab/javateaserver/models/User.java
@@ -2,6 +2,15 @@ package org.nittalab.javateaserver.models;
 
 public class User {
 
+    private String uid;
+    private String name;
+    private String pw;
+    private String university;
+    private String faculty;
+    private String department;
+    private int grade;
+
+
     //UserのTokenを返すメゾット
     public String getToken(){
         return null;
