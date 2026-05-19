4bd923c5b0f34bc53e23dc67e1ba2f164f3c5ec7
h-maeda <s2371087@s.konan-u.ac.jp>
2026-05-19 14:04:44 +0900

    メソッドを作成した。

--- src/main/java/org/nittalab/javateaserver/models/University.java
+++ src/main/java/org/nittalab/javateaserver/models/University.java
@@ -1,4 +1,12 @@
 package org.nittalab.javateaserver.models;
 
+
 public class University {
+    public Faculty createFaculty(String faculty_name){
+        return null;
+    }
+
+    public Faculty getFaculty(String faculty_name){
+        return null;
+    }
 }
--- src/main/java/org/nittalab/javateaserver/repositories/CategoryRepository.java
+++ src/main/java/org/nittalab/javateaserver/repositories/CategoryRepository.java
@@ -1,4 +1,17 @@
 package org.nittalab.javateaserver.repositories;
 
+import org.nittalab.javateaserver.models.University;
+import org.springframework.stereotype.Repository;
+import java.util.*;
+
+@Repository
 public class CategoryRepository {
+    public University createUniversity(String name, String kana) {
+        return null;
+    }
+
+    public University getUniversity(String univ_id) {
+        return null;
+    }
+
 }
