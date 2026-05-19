2fb216dc317b0592ee7ef77216a5d65fd433cd37
r-hashime <s2371079@s.konan-u.ac.jp>
2026-05-19 14:22:44 +0900

    UserRepository.javaにcreateUserメゾットとgetUserメゾットの外側を作成しました。 User.javaにgetTokenメゾットの外側を作成しました。

--- src/main/java/org/nittalab/javateaserver/models/User.java
+++ src/main/java/org/nittalab/javateaserver/models/User.java
@@ -1,4 +1,9 @@
 package org.nittalab.javateaserver.models;
 
 public class User {
+
+    //UserのTokenを返すメゾット
+    public String getToken(){
+        return null;
+    }
 }
--- src/main/java/org/nittalab/javateaserver/repositories/UserRepository.java
+++ src/main/java/org/nittalab/javateaserver/repositories/UserRepository.java
@@ -1,4 +1,18 @@
 package org.nittalab.javateaserver.repositories;
 
+import org.nittalab.javateaserver.models.User;
+import org.springframework.stereotype.Repository;
+
+@Repository
 public class UserRepository {
+
+    //Userクラスの作成
+    public User createUser(String uid, String name, String pw){
+        return null;
+    }
+
+    //Userクラスの取得
+    public User getUser(String uid){
+        return null;
+    }
 }
