95e1741c005c04b35d9b4f08b06e11a5da5a9655
m-nakasu <s2371098@s.konan-u.ac.jp>
2026-05-19 16:19:03 +0900

    学部一覧取得GETの範囲です。

--- src/main/java/org/nittalab/javateaserver/JavateaServerApplication.java
+++ src/main/java/org/nittalab/javateaserver/JavateaServerApplication.java
@@ -1,13 +1,65 @@
-package org.nittalab.javateaserver;
-
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
-public class JavateaServerApplication {
-
-    public static void main(String[] args) {
-        SpringApplication.run(JavateaServerApplication.class, args);
-    }
-
-}
+//package org.nittalab.javateaserver;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.springframework.stereotype.Component;
+//
+//import javax.ws.rs.GET;
+//import javax.ws.rs.Produces;
+//import javax.ws.rs.WebApplicationException;
+//import javax.ws.rs.core.Response;
+//
+//@SpringBootApplication
+//public class JavateaServerApplication {
+//
+//    public static void main(String[] args) {
+//        SpringApplication.run(JavateaServerApplication.class, args);
+//    }
+//
+//}
+//
+//@Path("/categories/universities/{univ-id}/faculties")
+//@Component
+//public class CategoryFacultyResource{
+//
+//    private LectureRepository lectureRepository = null;
+//    private CategoryRepository categoryRepository = null;
+//
+//    @Autowired
+//    public CategoryFacultyResource(LectureRepository lectureRepository, CategoryRepository categoryRepository) {
+//        this.lectureRepository = lectureRepository;
+//        this.categoryRepository = categoryRepository;
+//    }
+//
+//
+//    @GET
+//    @Produces(MediaType.APPLICATION_JSON)
+//
+//    public Response getFaculty(@PathParam("univ-id")String univId){
+//
+//        //400不正リクエスト
+//        if(univId == null || univId.isEmpty()){
+//            var response = Response.status(Response.Status.BAD_REQUEST).entity("不正なリクエスト");
+//            throw new WebApplicationException(response.build());
+//        }
+//
+//        //404データが存在しない
+//        if(facultyreRepository.getFaculty(univId) == null){
+//            var response = Response.status(Response.Status.NOT_FOUND).entity("データが存在しません");
+//            throw new WebApplicationException(response.build());
+//        }
+//
+//        //200成功
+//        ArrayList<String>faculty = facultyRepository.getFaculty(univId);
+//        if(faculty == null){
+//            return Response.status(Response.Status.NOT_FOUND).build();
+//        }
+//        //200成功
+//        return Response.status(Response.Status.OK).build();
+//
+//        //500予期せぬエラー
+//        //springbootが返してくれるためコード無し
+//    }
+//
+//}
