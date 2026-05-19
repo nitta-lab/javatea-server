ffc34faa6370cfd071102513be722cf8b830dd65
h-maeda <s2371087@s.konan-u.ac.jp>
2026-05-19 16:28:51 +0900

    メソッドの中身の一部を作成した。(CategoryRepository.javaは完成)

--- src/main/java/org/nittalab/javateaserver/models/University.java
+++ src/main/java/org/nittalab/javateaserver/models/University.java
@@ -1,8 +1,22 @@
 package org.nittalab.javateaserver.models;
 
-import org.nittalab.javateaserver.models.Faculty;
+import java.util.HashMap;
 
 public class University {
+    //関数は動詞名詞
+    //getは取ってくる(変更はなし)、setは設定(まるごと入れ替え)、putは一部書き換え,removeは削除
+    //addは引数に渡している場合、createは関数の中で新しく作ったものを追加する場合
+    private String faculty_name;
+    private String univ_id;
+    private String name;
+    private String kana;
+    private HashMap<String, Lecture> lectures = new HashMap<>();
+    private HashMap<String, Faculty> faculties = new HashMap<>();
+    public University(String univ_id, String name, String kana) { //クラス名と同じにする、初期化みたいなもの
+        this.univ_id = univ_id; //this.univ_idは、引数のuniv-id。
+        this.name = name;
+        this.kana = kana;
+    }
     public Faculty createFaculty(String faculty_name){
         return null;
     }
