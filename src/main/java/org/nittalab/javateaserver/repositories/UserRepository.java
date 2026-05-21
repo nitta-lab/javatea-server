565cde50e871ba92e77f92076aeb0e032a4d744d
r-hashime <s2371079@s.konan-u.ac.jp>
2026-05-21 15:03:26 +0900

    UserRepository.java：起動時のTest用メゾットを追加、getUserメゾットの実装、checkDuplicateメゾットを追加

--- src/main/java/org/nittalab/javateaserver/repositories/UserRepository.java
+++ src/main/java/org/nittalab/javateaserver/repositories/UserRepository.java
@@ -9,6 +9,12 @@ import java.util.HashMap;
 public class UserRepository {
     private final HashMap<String, User> userMap = new HashMap<>(); //User情報を持つHashMap
 
+    //test用(起動時にユーザ登録)
+    public UserRepository(){
+        User user0 = new User("test01", "testUser01", "testpass01");
+        userMap.put(user0.getUid(), user0);
+    }
+
     //Userクラスの作成
     public User createUser(String uid, String name, String pw){
         User u = new User(uid, name, pw);
@@ -18,6 +24,13 @@ public class UserRepository {
 
     //Userクラスの取得
     public User getUser(String uid){
-        return null;
+        return userMap.get(uid);
     }
+
+    //Userが存在するかを返す(存在しない場合false)
+    public boolean checkDuplicate(String uid){
+        return userMap.containsKey(uid);
+    }
+
+    //Tokenの生成
 }
