785e190b4a7b1b902bd771ec73cb2fc56ecb5bbd
paison-suto5 <140292494+paison-suto5@users.noreply.github.com>
2026-05-26 13:57:04 +0900

    ooooooo????

--- src/main/java/org/nittalab/javateaserver/resources/CategoryUniversityResource.java
+++ src/main/java/org/nittalab/javateaserver/resources/CategoryUniversityResource.java
@@ -5,6 +5,8 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 import org.nittalab.javateaserver.models.University;
+import org.nittalab.javateaserver.repositories.CategoryRepository;
+import org.nittalab.javateaserver.repositories.LectureRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -73,13 +75,14 @@ public class CategoryUniversityResource {
 
         // 200 OK
         //getUnivNameKana→指定した大学情報（読みとかなを取得）
-        return categoryRepository.getUniversity();
+        return categoryRepository.getUniversity(univId);
     }
 
     //大学名を登録・更新→指定された大学IDに対応する大学名を登録または更新する。
     @PUT
     @Path("/{univ-id}/name")
     @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+    @Produces(MediaType.TEXT_PLAIN)
     public  void updateUnivName(@PathParam("univ-id") String univId, @FormParam("name") String name) {
         //404エラー
         if (univId == null || name == null) {
@@ -91,15 +94,16 @@ public class CategoryUniversityResource {
 
         //200登録成功putUnivName
         //setName
+        categoryRepository.setName(univId, name);
     }
 
     //大学の読み仮名変更
     @PUT
     @Path("/{univ-id}/kana")
     @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
-    public  void updateUnivKana(@PathParam("univ-id") String univId, @FormParam("name") String name) {
+    public  void updateUnivKana(@PathParam("univ-id") String univId, @FormParam("Kana") String kana) {
         //404エラー
-        if (univId == null || name == null) {
+        if (univId == null || kana == null) {
 
             throw new WebApplicationException(
                     Response.status(Response.Status.BAD_REQUEST)
@@ -108,6 +112,7 @@ public class CategoryUniversityResource {
         }
         //200登録成功putUnivKana
         //setKana
+        return categoryRepository.setKana(univId, kana);
     }
 
     //大学全般に属する科目一覧（ID)の取得。
@@ -125,7 +130,7 @@ public class CategoryUniversityResource {
         }
 
         //getLectIdで授業IDを取得
-        return lectureRepository.getLectures();
+        return categoryRepository.getLectures(univId);
     }
 
     @PUT
@@ -139,9 +144,18 @@ public class CategoryUniversityResource {
                             .entity("データが存在しません")
                             .build());
         }
+        // lectureRepositoryで指定されたlecture-idが存在するか確認する
+//        if (!lectureRepository.existsLecture(lectId)) {
+//            throw new WebApplicationException(
+//                    Response.status(Response.Status.NOT_FOUND)
+//                            .entity("指定された授業IDが存在しません")
+//                            .build());
+//        }
 
         //putLectIdで大学全般科目の授業IDを追加
+        //エラーチェックでレクチャーリポジトリ―で
         //addLecture
+        categoryRepository.addLecture(univId, lectId);
     }
 
 }
