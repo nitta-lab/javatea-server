e0c3b4af11a5e4f89fae33dc3b9b5f3b379d2c17
h-maeda <s2371087@s.konan-u.ac.jp>
2026-05-26 13:46:47 +0900

    test用のクラスを作成した。

--- src/test/java/org/nittalab/javateaserver/repositories/TestCategoryRepository.java
+++ src/test/java/org/nittalab/javateaserver/repositories/TestCategoryRepository.java
@@ -2,14 +2,18 @@ package org.nittalab.javateaserver.repositories;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
 import java.util.*;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 public class TestCategoryRepository {
+
     @Test
     void CategoryRepositoryTest() {
-        CategoryRepository categoryRepository = new CategoryRepository();
 
+
+        CategoryRepository categoryRepository = new CategoryRepository();
     }
 }
