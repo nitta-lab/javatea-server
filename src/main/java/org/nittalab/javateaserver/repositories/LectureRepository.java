5e6083034dbae0ca4e2d23ed93d1d442685806d3
s-machida <s2371089@s.konan-u.ac.jp>
2026-06-30 16:04:18 +0900

    LectureResourceに例を追加して、Timetableにも追加しました

--- src/main/java/org/nittalab/javateaserver/repositories/LectureRepository.java
+++ src/main/java/org/nittalab/javateaserver/repositories/LectureRepository.java
@@ -11,7 +11,14 @@ import java.util.Set;
 public class LectureRepository {
 
     private HashMap<String, Lecture> lectureMap = new HashMap<>();
-
+    public LectureRepository(){
+        createLecture("オブジェクト指向プログラミング", 3, "前期", 1, "月", 2);
+        createLecture("ソフトウェア工学", 3, "前期", 1, "火", 2);
+        createLecture("ロボティクス", 3, "前期", 1, "水", 2);
+        createLecture("コンパイラインタプリタ", 3, "後期", 1, "水", 5);
+        createLecture("基礎体育学演習", 1, "通年", 2, "木", 1);
+        createLecture("AI・データサイエンス入門", 1, "その他", 1, "土", 3);
+    }
     //授業の追加
     public String createLecture(String name, int grade, String semester, int frame, String day, int period) {
         Set<String> keyList = lectureMap.keySet();
