fa1a378c143b55f7dfe7ce3290888f3ab50890c3
k-nomura <s2371086@s.konan-u.ac.jp>
2026-07-09 16:13:25 +0900

    AnswerとAnswerRepositoryを調整

--- src/main/java/org/nittalab/javateaserver/models/Answer.java
+++ src/main/java/org/nittalab/javateaserver/models/Answer.java
@@ -8,23 +8,15 @@ public class Answer {
     private String uid;
 
     //コンストラクタ
-    public Answer(String aid, String body, String uid) {
+    public Answer(String aid, String body,String uid) {
         this.aid = aid;
-        this.body = new String();
-        this.uid = new String();
-    }
-
-    public String getAid() {return aid;}; //aidを返す(念のため)
-
-    public String getBody() {return body;};
-
-    public void setBody(String body) {
         this.body = body;
-    }
-
-    public String getUid() {return uid;};
-
-    public void setUid(String uid) {
         this.uid = uid;
     }
+
+    public String getAid() {return aid;} //aidを返す(念のため)
+
+    public String getBody() {return body;}
+
+    public String getUid() {return uid;}
 }
--- src/main/java/org/nittalab/javateaserver/repositories/AnswerRepository.java
+++ src/main/java/org/nittalab/javateaserver/repositories/AnswerRepository.java
@@ -1,7 +1,39 @@
 package org.nittalab.javateaserver.repositories;
 
+import org.nittalab.javateaserver.models.Answer;
 import org.springframework.stereotype.Repository;
+
+import java.util.HashMap;
 
 @Repository
 public class AnswerRepository {
+
+    private HashMap<String, HashMap <String, Answer>> answers = new HashMap<>(); //(qid, (aid, answer))という入れ子構造
+
+    public Answer createAnswer(String qid, String body, String uid) {
+        int size = answers.size() + 1;
+        String aid = "aid" + size;
+        HashMap<String, Answer> answer = new HashMap<>();
+
+        answer.put(aid, new Answer(qid, body, uid));
+        answers.put(qid, answer);
+
+        return answers.get(qid).get(aid);
+    }
+
+    public HashMap <String, Answer> getAnswers(String qid) {
+        if (!answers.containsKey(qid)) {
+            return null;
+        }
+
+        return answers.get(qid);
+    }
+
+    public Answer getAnswer(String qid, String aid) {
+        if (!answers.get(qid).containsKey(aid)) {
+            return null;
+        }
+
+        return answers.get(qid).get(aid);
+    }
 }
