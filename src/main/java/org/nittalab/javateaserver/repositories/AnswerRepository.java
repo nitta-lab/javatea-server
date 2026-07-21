b4a6b08d40930a2cf3840e84e62a53937bc0f060
t-yokoyama <s2371093@s.konan-u.ac.jp>
2026-07-21 14:02:40 +0900

    answerのテスト用データを作ろうとしている(上手くいってない)

--- src/main/java/org/nittalab/javateaserver/repositories/AnswerRepository.java
+++ src/main/java/org/nittalab/javateaserver/repositories/AnswerRepository.java
@@ -10,6 +10,18 @@ public class AnswerRepository {
 
     private HashMap<String, HashMap <String, Answer>> answers = new HashMap<>(); //(qid, (aid, answer))という入れ子構造
 
+    public AnswerRepository() {
+        // test用
+        createAnswer("qid1", "×まず図書館の中に入ってください。そのあと、左手に向かって進めばとりあえずパソコンはあります", "test02");
+        createAnswer("qid1", "〇aid2分からん。すまんかった。", "test03");
+        createAnswer("qid2", "×やっぱカレーでーす。以上でーす", "test04");
+        createAnswer("qid2", "〇aid3実は…うどんが一番うまいんです！うどんならどれでもおいしいよ！", "test03");
+        createAnswer("qid3", "×オーバーライドは子供が親を上書きすんねん！", "test03");
+        createAnswer("qid3", "〇aid4共通の機能を持つ「親クラス」を引き継ぎつつ、特定の「子クラス」だけ一部の動作を変えたい場合に、同じ名前のメソッドを再定義して元の機能を上書きします。", "test04");
+        createAnswer("qid4", "×分かりません。すまんかったです。", "test02");
+        createAnswer("qid4", "〇aid5あるクラス（設計図）が持っている属性や機能を、別の新しいクラスに引き継がせる仕組み", "test04");
+    }
+
     public Answer createAnswer(String qid, String body, String uid) { //解答の作成
         int size = answers.size() + 1;
         String aid = "aid" + size;
